6089af53910dd0d7fe9d2373cee7eb0d40bc2f7b
Melanie2123 <orralamelanie4@gmail.com>
2025-01-11 15:35:13 -0500

    test 2

--- Proyectotransaccional/Proyectotransaccional/Menu.Designer.cs
+++ Proyectotransaccional/Proyectotransaccional/Menu.Designer.cs
@@ -31,13 +31,15 @@
             button1 = new Button();
             checkBox1 = new CheckBox();
             label1 = new Label();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(516, 129);
+            button1.Location = new Point(452, 97);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(82, 22);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
@@ -45,9 +47,10 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(307, 121);
+            checkBox1.Location = new Point(269, 91);
+            checkBox1.Margin = new Padding(3, 2, 3, 2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(101, 24);
+            checkBox1.Size = new Size(83, 19);
             checkBox1.TabIndex = 1;
             checkBox1.Text = "checkBox1";
             checkBox1.UseVisualStyleBackColor = true;
@@ -55,20 +58,31 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(348, 203);
+            label1.Location = new Point(304, 152);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 2;
             label1.Text = "label1";
             // 
+            // button2
+            // 
+            button2.Location = new Point(368, 40);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Menu
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(checkBox1);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Menu";
             Text = "Menu";
             ResumeLayout(false);
@@ -80,5 +94,6 @@
         private Button button1;
         private CheckBox checkBox1;
         private Label label1;
+        private Button button2;
     }
 }
